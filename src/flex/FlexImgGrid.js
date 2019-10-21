f64ddc926b234d57e5cdf7fa69b0abc457636fe5
Alex Hays <alexhays@gmail.com>
2019-10-21 18:16:18 -0400

    asdf

--- src/flex/FlexImgGrid.js
+++ src/flex/FlexImgGrid.js
@@ -6,14 +6,14 @@ export const FlexImgGrid = (props) => (
   <>
     <h1> Flex Img Grid </h1>
     <div className="container img-grid">
-      <div><img src="https://picsum.photos/200"></img></div>
-      <div><img src="https://picsum.photos/300/200"></img></div>
-      <div><img src="https://picsum.photos/400"></img></div>
-      <div><img src="https://picsum.photos/200/100"></img></div>
-      <div><img src="https://picsum.photos/200/300"></img></div>
-      <div><img src="https://picsum.photos/600/200"></img></div>
-      <div><img src="https://picsum.photos/200"></img></div>
-      <div><img src="https://picsum.photos/800"></img></div>
+      <div className="normal"><img src="https://picsum.photos/200"></img></div>
+      <div className="normal"><img src="https://picsum.photos/300/200"></img></div>
+      <div className="big"><img src="https://picsum.photos/400"></img></div>
+      <div className="normal"><img src="https://picsum.photos/200/100"></img></div>
+      <div className="big"><img src="https://picsum.photos/800"></img></div>
+      <div className="normal"><img src="https://picsum.photos/200/300"></img></div>
+      <div className="big"><img src="https://picsum.photos/600/200"></img></div>
+      <div className="normal"><img src="https://picsum.photos/200"></img></div>
     </div>
   </>
 )
